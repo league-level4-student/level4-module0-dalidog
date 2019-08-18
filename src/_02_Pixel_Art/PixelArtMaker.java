9e32bbfd30d7982cda2ade6598742d1768d42440
League Student <league@iMac-40.attlocal.net>
2019-08-18 11:31:31 -0700

    savestate

--- src/_02_Pixel_Art/PixelArtMaker.java
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -4,27 +4,32 @@ import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class PixelArtMaker implements MouseListener{
 	private JFrame window;
 	private GridInputPanel gip;
 	private GridPanel gp;
+	private JButton save;
 	ColorSelectionPanel csp;
-	
+	int r;
+	int c;
 	public void start() {
 		gip = new GridInputPanel(this);	
 		window = new JFrame("Pixel Art");
 		window.setLayout(new FlowLayout());
 		window.setResizable(false);
-		
+		save=new JButton("Save State");
+		window.add(save);
+		save.addMouseListener(this);
 		window.add(gip);
 		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 	}
 
-	public void submitGridData(int w, int h, int r, int c) {
+	public void submitGridData(int w, int h,r, c) {
 		gp = new GridPanel(w, h, r, c);
 		csp = new ColorSelectionPanel();
 		window.remove(gip);
@@ -41,6 +46,11 @@ public class PixelArtMaker implements MouseListener{
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		if(e.getSource()==save) {
+			for (int i = 0; i < ; i++) {
+				
+			}
+		}
 	}
 
 	@Override
@@ -49,6 +59,7 @@ public class PixelArtMaker implements MouseListener{
 		System.out.println(csp.getSelectedColor());
 		gp.clickPixel(e.getX(), e.getY());
 		gp.repaint();
+		
 	}
 
 	@Override
